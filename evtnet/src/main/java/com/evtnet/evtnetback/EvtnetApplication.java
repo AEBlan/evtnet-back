b4614129ca88295c115437499f41a72e64cc86c9
AEBlan <94871121+AEBlan@users.noreply.github.com>
2025-07-04 19:07:22 -0300

    Inicialización

---
+++ evtnet/src/main/java/com/evtnet/evtnetback/EvtnetApplication.java
@@ -0,0 +1,13 @@
+package com.evtnet.evtnetback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvtnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvtnetApplication.class, args);
+	}
+
+}
---
+++ evtnet/src/test/java/com/evtnet/evtnetback/EvtnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evtnet.evtnetback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvtnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
