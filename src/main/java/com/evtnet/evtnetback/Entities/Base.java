14cb468b92999c745c4ca7846c5d5ca6d737ec66
AlbinoSergio <sergioalbino@gmail.com>
2025-07-18 17:30:25 -0300

    Inicio de estructura mvc

---
+++ src/main/java/com/evtnet/evtnetback/Entities/Base.java
@@ -0,0 +1,23 @@
+package com.evtnet.evtnetback.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Base implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaBaja;
+}
